e270045e8e2c32c67331ea7e1cc0054955ce2173
exdjamm <25414650+exodojaffar@users.noreply.github.com>
2025-11-08 13:04:26 -0400

    force-commit: changes on spaces on save

--- parser/principal.cpp
+++ parser/principal.cpp
@@ -1,17 +1,17 @@
 #include "parser.h"
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
-    //Verifica se foi executado corretamente
-    //Essa main espera receber o nome do arquivo a ser
-    //executado na linha de comando.
+    // Verifica se foi executado corretamente
+    // Essa main espera receber o nome do arquivo a ser
+    // executado na linha de comando.
     if (argc != 2)
     {
-        cout<< "Uso: ./compiler nome_arquivo.mj\n";
+        cout << "Uso: ./compiler nome_arquivo.mj\n";
         return 1;
     }
 
-    Parser* parser = new Parser(argv[1]);
+    Parser *parser = new Parser(argv[1]);
 
     parser->run();
 
